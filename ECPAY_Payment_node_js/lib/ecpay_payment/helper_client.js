526f5726a669ba592b388cf472d341ee151dc1c6
Wesley <wesley100002001@gmail.com>
2021-05-31 15:06:41 +0800

    Isolate get_check_mac_value function

--- ECPAY_Payment_node_js/lib/ecpay_payment/helper_client.js
+++ ECPAY_Payment_node_js/lib/ecpay_payment/helper_client.js
@@ -10,32 +10,36 @@ class ECpayHelperClient {
         this.verify_aiochkout = new verify.AioCheckOutParamVerify();
     }
 
-  get_check_mac_value(period_info, parameters, invoice={}) {
-      let unsupport = ['IgnorePayment', 'Redeem', 'CreditInstallment', 'InstallmentAmount'];
-          this.payment_client._aiochkout_base_proc(parameters, invoice, unsupport, 'Credit');
-          if (period_info.constructor === Object){
-              let period_args = ['PeriodAmount', 'PeriodType', 'Frequency', 'ExecTimes', 'PeriodReturnURL'];
-              period_args.sort().forEach(function (pname) {
-                  if (Object.keys(period_info).sort().indexOf(pname, 0) === -1){
-                      throw new Error(`Credit card period parameters must be ${period_args}.`);
-                  }
-              });
-              Object.assign(parameters, period_info);
-                  // Add total amount protection!!!
+    get_check_mac_value_before_payment(period_info, parameters, invoice={}) {
+        let unsupport = ['IgnorePayment', 'Redeem', 'CreditInstallment', 'InstallmentAmount'];
+        this.payment_client._aiochkout_base_proc(parameters, invoice, unsupport, 'Credit');
+        if (period_info.constructor === Object){
+            let period_args = ['PeriodAmount', 'PeriodType', 'Frequency', 'ExecTimes', 'PeriodReturnURL'];
+            period_args.sort().forEach(function (pname) {
+                if (Object.keys(period_info).sort().indexOf(pname, 0) === -1){
+                    throw new Error(`Credit card period parameters must be ${period_args}.`);
+                }
+            });
+            Object.assign(parameters, period_info);
+                // Add total amount protection!!!
 
-              this.verify_aiochkout.verify_aio_payment_param(parameters);
-              // encode special param
-              let sp_param = this.verify_aiochkout.get_special_encode_param('AioCheckOut');
-              this.helper.encode_special_param(parameters, sp_param);
-      
-              // Insert chkmacval
-              // console.log(params);
-              let chkmac = this.helper.gen_chk_mac_value(parameters);
-              
-              return chkmac;
-          } else {
-              throw new Error(`Received period_info argument must be a Object.`);
-          }
+            this.verify_aiochkout.verify_aio_payment_param(parameters);
+            // encode special param
+            let sp_param = this.verify_aiochkout.get_special_encode_param('AioCheckOut');
+            this.helper.encode_special_param(parameters, sp_param);
+    
+            // Insert chkmacval
+            // console.log(params);
+            let chkmac = this.helper.gen_chk_mac_value(parameters);
+            
+            return chkmac;
+        } else {
+            throw new Error(`Received period_info argument must be a Object.`);
+        }
+    }
+
+    get_check_mac_value(params) {
+        return this.helper.gen_chk_mac_value(parameters);
     }
 }
 
